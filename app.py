29ccfa540554850210878b20536afafddf7b78e8
NathBuiss <nb.nathan12@gmail.com>
2026-07-19 17:57:56 +0200

    fix: use db.session.get instead of legacy Query.get in load_user
    Query.get() is a legacy SQLAlchemy 1.x construct superseded by
    Session.get() in 2.0. Update the Flask-Login user loader accordingly.

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ class TimelineEvent(db.Model):
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
 
 # ---------------------
 # Forms
